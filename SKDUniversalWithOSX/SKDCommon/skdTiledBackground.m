a6faea9e942a81b98f6081832c5faf2f1265c32e
spritekitdesigner <skd@spritekitdesigner.com>
2014-01-30 17:04:44 +0100

    -tiled background support

--- SKDUniversalWithOSX/SKDCommon/skdTiledBackground.m
+++ SKDUniversalWithOSX/SKDCommon/skdTiledBackground.m
@@ -25,6 +25,8 @@
 				float py = (float)y * gridSize.height;
 				py = size.height - py - midY;
 				px = px - midX;
+				px += (gridSize.width/2);
+				py -= (gridSize.height/2);
 				NSString* texName = [NSString stringWithFormat:@"%@-%d",atlasName,j];
 				SKTexture* tex = [atlas textureNamed:texName];
 				SKSpriteNode *tile = [SKSpriteNode spriteNodeWithTexture:tex];
